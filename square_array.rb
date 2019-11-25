15cea873d8636e848fe6ecc69e3e412ba711a57f
Pedro Huertas <pmh242@gmail.com>
2019-11-25 06:38:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,14 @@
 def square_array(numbers)
+<<<<<<< HEAD
   new_numbers = []
   numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
+=======
+  array = []
+  new_numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+>>>>>>> 622eaf9a6d7586a00e90ec978fc0faf6c767aef8
 end
